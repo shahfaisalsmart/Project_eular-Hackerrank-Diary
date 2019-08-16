2228687d82603d19fbcd775ca732b161d4072310
Mohammad Faisal <shahfaisalsmart1234@gmail.com>
2019-08-17 01:17:13 +0530

    Solved Successfully

---
+++ smallestMultiple.cpp
@@ -0,0 +1,53 @@
+/*
+  Author: Mohammad Faisal
+  series: Project Euler
+  Project Euler #5: Smallest multiple
+  Dated: 17-Aug-2019
+*/
+
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+using namespace std;
+int gcd(int a,int b)
+{
+    if(b)
+        return gcd(b,a%b);
+    else 
+        return a;
+}
+int main(){
+    int t;
+    cin >> t;
+    for(int a0 = 0; a0 < t; a0++){
+        int n;
+        cin >> n;
+        
+        int ans = 1;
+        for(int i=1;i<=n;i++)
+            ans = (ans*i)/gcd(ans,i);
+        
+        cout<<ans<<endl;
+    }
+    return 0;
+}
+
